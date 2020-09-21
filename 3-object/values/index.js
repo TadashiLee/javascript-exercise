f2376fc27186838be168c8a715948ae0039e9f3c
TadashiLee <584638605@qq.com>
2020-09-21 20:12:49 +0800

    [wenchang.li]:implement 3-object

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,9 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  let sum = 0;
+  const numbers = Object.values(source);
+  numbers.forEach(num => {
+    sum += Number(num);
+  });
+  return sum;
 }
