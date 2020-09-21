dc9d757c536987fde033376aa6176a26b07cbdc6
TadashiLee <584638605@qq.com>
2020-09-21 20:59:48 +0800

    [wenchang.li]:implement 4-class prcatice 4,5

--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,18 @@
+import Person from './person';
+
 // TODO 20: 在这里写实现代码
+export default class Teacher extends Person {
+  constructor(name, age, klass) {
+    super();
+    this.name = name;
+    this.age = age;
+    this.klass = klass;
+  }
+
+  introduce() {
+    if (this.klass === undefined) {
+      return `My name is ${this.name}. I am ${this.age} years old. I am a Teacher. I teach No Class.`;
+    }
+    return `My name is ${this.name}. I am ${this.age} years old. I am a Teacher. I teach Class ${this.klass}.`;
+  }
+}
