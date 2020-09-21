589965348c3b8d88ce5534749ddbb5b9f0c7047f
TadashiLee <584638605@qq.com>
2020-09-21 21:30:30 +0800

    [wenchang.li]:implement 5-async

--- 5-async/3-fetch/index.js
+++ 5-async/3-fetch/index.js
@@ -1,6 +1,14 @@
 function fetchData(url) {
   // <-- start
   // TODO 23: 通过Fetch API实现异步请求
+  // eslint-disable-next-line consistent-return
+  fetch(url).then(respose => {
+    if (respose.status === 200) {
+      return respose.json;
+    }
+    // eslint-disable-next-line prefer-promise-reject-errors
+    Promise.reject('error msg');
+  });
   // end -->
 }
 
